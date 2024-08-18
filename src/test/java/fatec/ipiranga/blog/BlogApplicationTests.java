229f28a284ca5e3ddfaaf62494e65d96ea2a5e98
Gabriel Renato <gabrielrenatosc@gmail.com>
2024-08-17 22:02:31 -0300

    feat: first commit and project set ups

---
+++ src/test/java/fatec/ipiranga/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.ipiranga.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
